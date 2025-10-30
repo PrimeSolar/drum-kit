ee45abe2119d1b8a317f4e79c09bf5ad6182ec34
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-10-30 03:30:32 +0300

    Update meta-tags.js

--- scripts/meta-tags.js
+++ scripts/meta-tags.js
@@ -1,3 +1,18 @@
+/*
+ * Script
+ *
+ * Copyright © Vladislav Kazantsev
+ * All rights reserved.
+ * This code is the intellectual property of Vladislav Kazantsev.
+ * You are welcome to clone the related repository and use the code for exploratory purposes.
+ * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
+ * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
+ * Sharing the link to the file is permitted, except when directed toward retrieval purposes.
+ * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
+ * This copyright notice applies globally.
+ * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
+ */
+
 // Set the <html> element `lang` property
 const html = document.querySelector("html");
 html.lang = "en";
@@ -27,6 +42,20 @@ metaCopyright.name = "copyright";
 metaCopyright.content =
   "© " + new Date().getFullYear() + " Vladislav Kazantsev";
 document.querySelector("head").appendChild(metaCopyright);
+
+// Insert <meta name="description" content="The Drum Kit interactive web application allows users to play a virtual drum kit using their mouse or keyboard." />
+const metaDescription = document.createElement("meta");
+metaDescription.name = "description";
+metaDescription.content =
+  "The Drum Kit interactive web application allows users to play a virtual drum kit using their mouse or keyboard.";
+document.querySelector("head").appendChild(metaDescription);
+
+// Insert <meta name="keywords" content="drum, kit, interactive, web, application, play, virtual, mouse, keyboard, music" />
+const metaKeywords = document.createElement("meta");
+metaKeywords.name = "keywords";
+metaKeywords.content =
+  "drum, kit, interactive, web, application, play, virtual, mouse, keyboard, music";
+document.querySelector("head").appendChild(metaKeywords);
 
 // Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">
 const linkStyle = document.createElement("link");

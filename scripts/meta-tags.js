49f46329943dd94b3e1139ff9b08878aeb9872b1
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-11-10 00:35:04 +0300

    Update meta-tags.js
    Update the original version of the script.
    Improve the comments of the source code for better readability, maintenance, precision, consistency, and clarity.

--- scripts/meta-tags.js
+++ scripts/meta-tags.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Script
  *
  * Copyright © Vladislav Kazantsev
@@ -13,51 +13,51 @@
  * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
  */
 
-// Set the <html> element `lang` property
+/** Set the <html> element `lang` property. */
 const html = document.querySelector("html");
 html.lang = "en";
 
-// Set `dir="ltr"` for the <html> element
+/** Set `dir="ltr"` for the <html> element. */
 html.dir = "ltr";
 
-// Set <meta name="viewport" content="width=device-width, initial-scale=1" />
+/** Set <meta name="viewport" content="width=device-width, initial-scale=1" />. */
 const metaViewport = document.createElement("meta");
 metaViewport.name = "viewport";
 metaViewport.content = "width=device-width, initial-scale=1";
 document.querySelector("head").appendChild(metaViewport);
 
-// Set `title` for the <html> element based on a webpage's content
+// Set `title` for the <html> element based on a webpage's content. */
 const title = document.querySelector("h1");
 document.title = title.textContent.replace(" 🥁", "").trim();
 
-// Insert <meta name="author" content="Vladislav Kazantsev">
+/** Insert <meta name="author" content="Vladislav Kazantsev">
 const metaAuthor = document.createElement("meta");
 metaAuthor.name = "author";
 metaAuthor.content = "Vladislav Kazantsev";
-document.querySelector("head").appendChild(metaAuthor);
+document.querySelector("head").appendChild(metaAuthor); */
 
-// Insert <meta name="copyright" content="© `Current Year` Vladislav Kazantsev">
+/** Insert <meta name="copyright" content="© `Current Year` Vladislav Kazantsev">. */
 const metaCopyright = document.createElement("meta");
 metaCopyright.name = "copyright";
 metaCopyright.content =
   "© " + new Date().getFullYear() + " Vladislav Kazantsev";
 document.querySelector("head").appendChild(metaCopyright);
 
-// Insert <meta name="description" content="The Drum Kit interactive web application allows users to play a virtual drum kit using their mouse or keyboard." />
+/** Insert <meta name="description" content="The Drum Kit interactive web application allows users to play a virtual drum kit using their mouse or keyboard." />. */
 const metaDescription = document.createElement("meta");
 metaDescription.name = "description";
 metaDescription.content =
   "The Drum Kit interactive web application allows users to play a virtual drum kit using their mouse or keyboard.";
 document.querySelector("head").appendChild(metaDescription);
 
-// Insert <meta name="keywords" content="drum, kit, interactive, web, application, play, virtual, mouse, keyboard, music" />
+/** Insert <meta name="keywords" content="drum, kit, interactive, web, application, play, virtual, mouse, keyboard, music" />. */
 const metaKeywords = document.createElement("meta");
 metaKeywords.name = "keywords";
 metaKeywords.content =
   "drum, kit, interactive, web, application, play, virtual, mouse, keyboard, music";
 document.querySelector("head").appendChild(metaKeywords);
 
-// Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">
+/** Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">. */
 const linkStyle = document.createElement("link");
 linkStyle.rel = "stylesheet";
 linkStyle.type = "text/css";
@@ -67,26 +67,26 @@ linkStyle.onload = function () {
   document.body.style.display = "block";
 };
 
-// Insert <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Arvo">
+/** Insert <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Arvo">. */
 const linkFont = document.createElement("link");
 linkFont.rel = "stylesheet";
 linkFont.type = "text/css";
 linkFont.href = "https://fonts.googleapis.com/css?family=Arvo";
 document.querySelector("head").appendChild(linkFont);
 
-// Insert <link rel="icon" href="pics/logos/logo.svg">
+/** Insert <link rel="icon" href="pics/logos/logo.svg">. */
 const linkImage = document.createElement("link");
 linkImage.rel = "icon";
 linkImage.href = "assets/images/logo.svg";
 document.querySelector("head").appendChild(linkImage);
 
-// Set `type="text/javascript"` for all <script> elements for better compatibility
+/** Set `type="text/javascript"` for all <script> elements for better compatibility. */
 for (let x of document.querySelectorAll("script")) {
   x.type = "text/javascript";
 }
 
-// Insert <meta http-equiv="Pragma" content="no-cache">
-// var metaPragma = document.createElement("meta");
-// metaPragma.httpEquiv = "Pragma";
-// metaPragma.content = "no-cache";
-// document.querySelector("head").appendChild(metaPragma);
+/** Insert <meta http-equiv="Pragma" content="no-cache">
+var metaPragma = document.createElement("meta");
+metaPragma.httpEquiv = "Pragma";
+metaPragma.content = "no-cache";
+document.querySelector("head").appendChild(metaPragma); */
